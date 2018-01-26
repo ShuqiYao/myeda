89a7568fdc5135cdb7cee4ff4838481561d79dc4
Shuqi <shuqiyao2017@163.com>
2018-01-26 23:55:07 +0800

    DESENSITIVE

--- EDA18_0118/script.R
+++ EDA18_0118/script.R
@@ -3,9 +3,8 @@
 
 args<-commandArgs(T)
 source('source_list.R')
-# args <- c("dwd_intopieces_repair_cs","2018-01-15",".")
-s <- paste(" select * from ", args[1]," where etl_date = '",args[2],"' limit 10000",sep = "")
-data <- dbGetQuery(hiveconnection,s)
+## put input arguments here
+
 
 # SetNa(data)
 data[data == "NULL"] <- NA
